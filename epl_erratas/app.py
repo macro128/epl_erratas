8c295cf3c60b36ea87c501500c5ebebe5831cc46
macro128 <29261232+macro128@users.noreply.github.com>
2022-12-14 13:55:14 -0300

    fix app imports for streamlit deploy

--- epl_erratas/app.py
+++ epl_erratas/app.py
@@ -3,13 +3,10 @@ import streamlit as st
 try:
     from epl_erratas.src.libraries import KoboLibrary
 except ModuleNotFoundError:
-    import os
+    import sys
     from pathlib import Path
 
-    _python_path = os.getenv("PYTHONPATH", "")
-    os.environ[
-        "PYTHONPATH"
-    ] = f"{Path(__file__).parent.parent.absolute()}:{_python_path}"
+    sys.path.insert(0, str(Path(__file__).parent.parent.absolute()))
     from epl_erratas.src.libraries import KoboLibrary
 
 from epl_erratas.src.models import Book, Erratum
